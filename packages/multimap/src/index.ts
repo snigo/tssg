8f2991152aa8ac40d74d157bdd81cb015e9ed710
Igor Snitkin <snigo.ogins@gmail.com>
2023-10-28 00:12:33 +0200

    add exports to multimap package

--- packages/multimap/src/index.ts
+++ packages/multimap/src/index.ts
@@ -1,2 +1,4 @@
+export { default as AliasMap } from './alias.js';
 export { default as ArrayMultimap } from './array.js';
+export { default as Bimap } from './bimap.js';
 export { default as SetMultimap } from './set.js';
